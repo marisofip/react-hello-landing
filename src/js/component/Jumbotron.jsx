2224048047330013879b4f33d31c6d42e8c86d84
marisofip <marisofipalencia@gmail.com>
2022-12-14 20:19:56 +0000

    navbar y jumbo

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+
+function Jumbotron() {
+	return (
+		
+      <div className="p-5 container-fluid bg-light">
+        <h3 className="display-5 fw-bold">A Warm Welcome!</h3>
+        <p className="col-md-8 fs-4">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500.</p>
+        <button className="btn btn-primary btn-lg" type="button">Call to action</button>
+      </div>
+    
+		
+	);
+};
+
+export default Jumbotron;
---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Menu()  {
+	return (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid flex-row flex-wrap">
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span clclassNameass="navbar-toggler-icon"></span>
+      </button>
+      <div className=" collapse navbar-collapse" id="navbarNav">
+      
+          <ul className="navbar-nav flex-row flex-wrap ms-md-auto" >
+          <li className="nav-item">
+                  <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href="#">About</a>
+              </li>
+              <li classNameass="nav-item">
+                  <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href="#">Contact</a>
+              </li>
+          </ul>
+          
+      </div>
+  </div>
+</nav>
+
+    );
+}
+
+export default Menu;
